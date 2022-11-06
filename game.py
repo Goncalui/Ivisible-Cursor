fe1909b4f82dedae64c13fab2960732fc442c972
EMSJunior <jumendessouza1@gmail.com>
2022-11-06 15:03:26 -0300

    Pontução e invisivel

--- game.py
+++ game.py
@@ -8,30 +8,27 @@ class Dish:
         self.center = [random.randint(radius,screen.get_width() - radius),random.randint(radius,screen.get_height() - radius)]
         self.color = color
         self.radius = radius
+        
     def plotDish(self, x,y):
         pygame.draw.circle(self.screen, self.color, [self.center[0], self.center[1]] , self.radius)
         pygame.draw.circle(self.screen, (50, 50, 50), [self.center[0], self.center[1]] , self.radius, int(self.radius/5))
         pygame.draw.circle(self.screen, (80, 80, 80), [self.center[0], self.center[1]] , int(self.radius/2), int(self.radius/10))
         pygame.draw.circle(self.screen, (170, 0, 0), [self.center[0], self.center[1]] , int(self.radius/10))
-        self.getColision(x,y)
         
     def getColision(self, x, y):
         if (x - self.center[0])**2 + (y - self.center[1])**2 <= self.radius**2:
             self.radius -= int(self.radius/20)
             self.center = [random.randint(self.radius,self.screen.get_width() - self.radius),random.randint(self.radius,self.screen.get_height() - self.radius)]
-            pygame.draw.circle(self.screen,(255,0,0), [x,y], 10)  
             return True
         else:
             return False 
 
-
-class MouseElipse:
-    def __init__(self,screen, x,y, color = (255,0,0)):
-        self.screen = screen
-        self.x = x
-        self.y = y
-        self.color = color
-    def plotME(self,x,y):
-        
+class Points:
+    def __init__(self):
+         self.value = 0
+    def Add(self, qtd = 1):
+        self.value += qtd
+    def Clear(self):
+        self.value = 0
     
         
--- main.py
+++ main.py
@@ -21,31 +21,49 @@ fps = 144
 
 clock = pygame.time.Clock()
 
+pygame.font.init()
+comicSans = pygame.font.SysFont('Comic Sans MS', 30)
+
+points = game.Points()
+
 
 playStartPos = []
 
 circle1, circle2, circle3 = [game.Dish(screen), game.Dish(screen), game.Dish(screen)]
 
-while running:    
+counter = 0
+while running: 
     x = -1
     y = -1
-    
+
     screen.fill((255,255,255))
+    
+    screen.blit(comicSans.render("Pontuação: " + str(points.value), False, (0, 0, 0)), (5,5))
 
     
     
+    mousePos = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONUP:
             x = pygame.mouse.get_pos()[0]
             y = pygame.mouse.get_pos()[1]          
-            mousePos = pygame.mouse.get_pos()
             radius = int((- playStartPos[0][0] + playStartPos[0][1] )/2)
             center =  [playStartPos[0][0]+radius,playStartPos[1][0]+radius]
-            if( np.sqrt((mousePos[0]-center[0])**2 +(mousePos[1]-center[1])**2)< radius-7 ):
-                tela = 2
-                pygame.mouse.set_visible(False)
+            if tela == 0:
+                pygame.mouse.set_visible(True)
+                if( np.sqrt((mousePos[0]-center[0])**2 +(mousePos[1]-center[1])**2)< radius-7 ):
+                    tela = 2
+                    points.Clear()
+                    print('click')
+                    
+            elif tela == 2:
+                    
+                if circle1.getColision(x,y) or circle2.getColision(x,y) or circle3.getColision(x,y):
+                    points.Add()
+                else:
+                    tela = 0
     
     if(tela == 0):
         playStartPos = menu.menuPrincipal(screen)
@@ -53,12 +71,28 @@ while running:
         print('options')
     elif(tela == 2):
         #game
+        counter += 1
         circle1.plotDish(x,y)
         circle2.plotDish(x,y)
         circle3.plotDish(x,y)
+        
+        sec = counter/fps
+        if sec <= 1:
+            screen.blit(comicSans.render("3", False, (255, 0, 0)), [mousePos[0] - 3, mousePos[1] + 10 ])
+        elif sec <= 2:
+            screen.blit(comicSans.render("2", False, (255, 0, 0)), [mousePos[0] - 3, mousePos[1] + 10 ])
+        elif sec <= 3:
+            screen.blit(comicSans.render("1", False, (255, 0, 0)), [mousePos[0] - 3, mousePos[1] + 10 ])
+        elif sec >= 3:
+                    pygame.mouse.set_visible(False)
+        if sec >=3 and sec <= 5:
+            screen.blit(comicSans.render("Invisivel", False, (255, 0, 0)), [mousePos[0] - 50, mousePos[1] + 10 ])
+        
     else:
-        print('game over')
-   
+        tela = 1
+        
+
+
     pygame.display.flip()
     clock.tick(fps)
 
