332d98ff8ff64dc36cd1df03aeb17116ae1ed8e4
EMSJunior <jumendessouza1@gmail.com>
2022-11-06 14:08:39 -0300

    pull madrugada

--- main.py
+++ main.py
@@ -17,43 +17,47 @@ rectSize2 = rectSize/4
 screenWidth = screen.get_width()
 
 tela = 0
-fps = 2
+fps = 144
 
 clock = pygame.time.Clock()
 
 
 playStartPos = []
 
-screen.fill((0,0,0))
+circle1, circle2, circle3 = [game.Dish(screen), game.Dish(screen), game.Dish(screen)]
 
 while running:    
-    x = pygame.mouse.get_pos()[0]
-    y = pygame.mouse.get_pos()[1]
+    x = -1
+    y = -1
     
-    game.game(screen)
-    atuali+=1
     screen.fill((255,255,255))
 
+    
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONUP:
+            x = pygame.mouse.get_pos()[0]
+            y = pygame.mouse.get_pos()[1]          
+            mousePos = pygame.mouse.get_pos()
+            radius = int((- playStartPos[0][0] + playStartPos[0][1] )/2)
+            center =  [playStartPos[0][0]+radius,playStartPos[1][0]+radius]
+            if( np.sqrt((mousePos[0]-center[0])**2 +(mousePos[1]-center[1])**2)< radius-7 ):
+                tela = 2
+                pygame.mouse.set_visible(False)
+    
     if(tela == 0):
         playStartPos = menu.menuPrincipal(screen)
     elif(tela == 1):
         print('options')
     elif(tela == 2):
         #game
-        print("on game")
+        circle1.plotDish(x,y)
+        circle2.plotDish(x,y)
+        circle3.plotDish(x,y)
     else:
         print('game over')
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.MOUSEBUTTONUP:
-            mousePos = pygame.mouse.get_pos()
-            radius = int((- playStartPos[0][0] + playStartPos[0][1] )/2)
-            center =  [playStartPos[0][0]+radius,playStartPos[1][0]+radius]
-            if( np.sqrt((mousePos[0]-center[0])**2 +(mousePos[1]-center[1])**2)< radius-7 ):
-                tela = 2
-   
    
     pygame.display.flip()
     clock.tick(fps)
